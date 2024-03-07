5652a269ee8a6b1e31658b0e858abef913c803bd
George Kibue <162186946+14georgeant@users.noreply.github.com>
2024-03-07 11:19:44 +0300

    Add files via upload
    Question 6 I used python IDLE.

---
+++ question6.py
@@ -0,0 +1,13 @@
+def count_vowels(sentence):
+    vowels = "aeiouAEIOU"
+    vowel_count = 0
+
+    for char in sentence:
+        if char in vowels:
+            vowel_count += 1
+
+    return vowel_count
+
+input_sentence = input("Enter a sentence: ")
+num_vowels = count_vowels(input_sentence)
+print("Number of vowels in the sentence:", num_vowels)
